dfa28cb2cf7118fe68f868801cd2eeb4782bceb5
Val <v.berbenetz@gmail.com>
2016-02-18 17:05:44 -0500

    Changed port

--- cocoacouriers_server.js
+++ cocoacouriers_server.js
@@ -137,10 +137,10 @@ https.createServer({
         '!CAMELLIA'
     ].join(':'),
     honorCipherOrder: true
-}, app).listen(8443);
+}, app).listen(443);
 
 // Needed for redirect to HTTPS
 http.createServer(function (req, res) {
     res.writeHead(301, {'Location': 'https://' + req.headers['host'] + req.url});
     res.end();
-}).listen(8080);
+}).listen(80);
